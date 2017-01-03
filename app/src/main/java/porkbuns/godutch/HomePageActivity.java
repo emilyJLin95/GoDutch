97e0498a8272f8c0e3da35317e482ee2a5807fa2
Emily Lin <emilylin@emilys-mbp-2.wireless.dartmouth.edu>
2017-01-02 16:55:15 -0800

    button layout

--- app/src/main/java/porkbuns/godutch/HomePageActivity.java
+++ app/src/main/java/porkbuns/godutch/HomePageActivity.java
@@ -1,13 +1,28 @@
 package porkbuns.godutch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void startProfile(View view) {
+        Log.v(HomePageActivity.class.getName(), "startProfile");
+    }
+
+    public void startMeal(View view) {
+    }
+
+    public void startHistory(View view) {
+    }
+
+    public void startFriends(View view) {
     }
 }
